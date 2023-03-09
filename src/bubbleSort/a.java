58bb55748fe65b4dd27de1c2d99c785ca6f50c0e
Omer Isik <isikomer4545@gmail.com>
2023-03-09 22:37:01 +0100

    TryCore

---
+++ src/bubbleSort/a.java
@@ -0,0 +1,38 @@
+package bubbleSort;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class a {
+
+    char c;
+    boolean b;
+    float f;
+    char a;
+
+    void print(){
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(f);
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        char c = 'a';
+        boolean b = false;
+        System.out.println(c);
+
+        List<String> arr = new ArrayList<>();
+        List<String> list = new LinkedList<>();
+        arr.add("asdsadas");
+        arr.add("aa");
+        arr.add("qwdqwdq");
+        arr.add(null);
+        arr.remove(null);
+        list.add(null);
+        System.out.println(list);
+
+        System.out.println(arr);
+    }
+}
